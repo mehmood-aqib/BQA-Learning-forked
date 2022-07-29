bbd7b33e7ba2c8dd106ce9ff2a6d68d16a60a63a
mehmood-aqib <aqib@xord.com>
2022-07-29 17:42:36 +0500

    Added ERC-20 token testcases

--- Kol-Staking/contracts/KOL.sol
+++ Kol-Staking/contracts/KOL.sol
@@ -6,7 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract KOLNET is ERC20 {
     constructor() ERC20("KOLNET", "KOL") {}
 
-    function mint(address to, uint256 amount) external  {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+
+    
 }
